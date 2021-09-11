f779c052154791cc39d2a9be9932e1329150c5a3
S. Jansen <sa.jansen1@gmail.com>
2021-09-11 14:57:12 +0200

    feat(settings): Reload settings if changed. feat(settings): Implemented option to reset authentication

--- source/screens/SettingsScreen.tsx
+++ source/screens/SettingsScreen.tsx
@@ -1,27 +1,42 @@
-import React, { useEffect } from "react";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 import { RefreshControl, ScrollView, StyleSheet, Text, TouchableOpacity } from "react-native";
 import Settings from "../scripts/settings";
 import { AccessRequestStatus, ServerAuth } from "../scripts/server/auth";
+import ConfirmationModal from "../components/ConfirmationModal";
+import { useFocusEffect } from "@react-navigation/native";
 
 interface SettingProps {
   name: string;
   sKey?: string;
   value?: any;
-  onPress?: (key?: string) => void;
+  onPress?: (setValue: (newValue: any) => void, key?: string) => void;
+  valueRender?: (value: any) => string;
 }
 
 const Setting: React.FC<SettingProps> =
-  ({ name, sKey, value, onPress = undefined }) => {
+  ({ name, sKey, value, onPress = undefined, valueRender = (it) => it.toString() }) => {
     if (value === undefined && sKey !== undefined) {
       value = Settings.get(sKey);
     }
 
+    const [_value, _setValue] = useState(value);
+    const setValue = (newValue: any) => {
+      if (sKey !== undefined) {
+        // @ts-ignore
+        Settings[sKey] = newValue;
+        // @ts-ignore
+        _setValue(Settings[sKey]);
+      } else {
+        _setValue(newValue);
+      }
+    };
+
     return (
       <TouchableOpacity
         style={styles.settingContainer}
-        onPress={onPress === undefined ? undefined : () => onPress(sKey)}>
+        onPress={onPress === undefined ? undefined : () => onPress(setValue, sKey)}>
         <Text style={styles.settingKey}>{name}</Text>
-        {value === undefined ? null : <Text style={styles.settingValue}>{value.toString()}</Text>}
+        {value === undefined ? null : <Text style={styles.settingValue}>{valueRender(_value)}</Text>}
       </TouchableOpacity>
     );
   };
@@ -31,37 +46,92 @@ interface ComponentProps {
 
 const SettingsScreen: React.FC<ComponentProps> = () => {
 
-  useEffect(() => {
-    return () => undefined;
-  }, [Settings]);
+  let confirmModalCallback: MutableRefObject<((isConfirmed: boolean) => void) | undefined> =
+    useRef(undefined);
+  const [confirmModalMessage, setConfirmModalMessage] = useState<string | undefined>(undefined);
+  const [isReloading, setReloading] = useState(false);
 
-  const reloadSettings = () => {
-    // todo...
+  const confirmModalCallbackWrapper = (isConfirmed: boolean) => {
+    setConfirmModalMessage(undefined);
+    confirmModalCallback.current?.(isConfirmed);
   };
 
-  let authenticationStatus = "No";
-  if (ServerAuth.isAuthenticated()) {
-    authenticationStatus = "Approved";
-  } else if (Settings.authStatus === AccessRequestStatus.DENIED) {
-    authenticationStatus = "Denied: " + Settings.authDeniedReason;
-  } else if (Settings.authStatus === AccessRequestStatus.REQUESTED) {
-    authenticationStatus = "Requested...";
+  const setConfirmModalCallback = (message: string | undefined, callback: ((isConfirmed: boolean) => void) | undefined) => {
+    if (message === undefined || callback === undefined) {
+      setConfirmModalMessage(undefined);
+      return;
+    }
+
+    confirmModalCallback.current = callback;
+    setConfirmModalMessage(message);
+  };
+
+  useFocusEffect(
+    React.useCallback(() => {
+      onFocus();
+      return onBlur;
+    }, [])
+  );
+
+  const onFocus = () => {
+    reloadSettings();
+  };
+
+  const onBlur = () => {
+  };
+
+
+  const reloadSettings = () => {
+    setReloading(true);
+    setTimeout(() => setReloading(false), 100);
+  };
+
+  function getAuthenticationStateAsMessage() {
+    if (ServerAuth.isAuthenticated()) {
+      return "Approved";
+    } else if (Settings.authStatus === AccessRequestStatus.DENIED) {
+      return "Denied: " + Settings.authDeniedReason;
+    } else if (Settings.authStatus === AccessRequestStatus.REQUESTED) {
+      return "Requested...";
+    }
+    return "No";
   }
+
+  let authenticationStatus = getAuthenticationStateAsMessage();
 
   return (
     <ScrollView
       style={styles.container}
-      refreshControl={<RefreshControl onRefresh={reloadSettings} refreshing={false} />}>
+      refreshControl={<RefreshControl onRefresh={reloadSettings} refreshing={isReloading} />}>
+
+      <ConfirmationModal isOpen={confirmModalMessage !== undefined}
+                         onClose={() => confirmModalCallbackWrapper(false)}
+                         onConfirm={() => confirmModalCallbackWrapper(true)}>
+        <Text>{confirmModalMessage}</Text>
+      </ConfirmationModal>
+
       {/*<Setting name={"Max input length"} sKey={"maxSearchInputLength"} />*/}
       {/*<Setting name={"Max. results to display"} sKey={"maxSearchResultsLength"} />*/}
       {/*<Setting name={"SongBundle API URL"} sKey={"songBundlesApiUrl"} />*/}
 
-      <Setting name={"Reset songs scale"} onPress={() => Settings.songScale = 1.0} />
-      {/*<Setting name={"Songs text size"} sKey={"songVerseTextSize"} />*/}
+      {isReloading ? null : <>
+        <Setting name={"Songs scale"} sKey={"songScale"}
+                 onPress={(setValue) => setValue(1.0)}
+                 valueRender={(it) => Math.round(it * 100) + " %"} />
 
-      <Setting name={"Use authentication with backend"} sKey={"useAuthentication"}
-               onPress={() => Settings.useAuthentication = !Settings.useAuthentication} />
-      <Setting name={"Authentication status with backend"} value={authenticationStatus} />
+        <Setting name={"Use authentication with backend"} sKey={"useAuthentication"}
+                 onPress={(setValue) => setValue(!Settings.useAuthentication)} />
+        <Setting name={"Authentication status with backend"} value={authenticationStatus}
+                 onPress={(setValue) =>
+                   setConfirmModalCallback(
+                     "Reset/forget authentication?",
+                     (isConfirmed) => {
+                       if (isConfirmed) {
+                         ServerAuth.forgetCredentials();
+                       }
+                       setValue(getAuthenticationStateAsMessage);
+                     })} />
+      </>}
     </ScrollView>
   );
 };
